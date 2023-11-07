b9138ec4d51c2c63bb8a0259419c1be02727a126
Rico <rico@cakeplabs.com>
2023-11-07 15:57:55 +0800

    EDP-41 enum utility for ROLE, and ACCESS

---
+++ src/utils/enum.utils.js
@@ -0,0 +1,13 @@
+const ROLE = {
+  USER: "USER",
+  SELLER: "SELLER",
+  ADMIN: "ADMIN",
+};
+
+const MODULE = {
+  AUTH: "AUTH",
+  LANDING: "LANDING",
+  TRANSACTION: "TRANSACTION",
+};
+
+module.exports = { ROLE, MODULE };
