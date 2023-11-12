46fe54a7352de99df3ed2b6a38d6d9aa6f4df76e
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-13 01:18:20 +0800

    EDP-48 update documentation config

--- src/routes/docs/index.js
+++ src/routes/docs/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const fs = require("fs");
 const YAML = require("yaml");
 const swaggerUi = require("swagger-ui-express");
-const file = fs.readFileSync("./src/docs/swagger.yml", "utf8");
+const file = fs.readFileSync("./src/docs/v1.swagger.yml", "utf8");
 const apiDocs = YAML.parse(file);
 const router = require("express").Router();
 
@@ -12,10 +12,10 @@ router.get(
   swaggerUi.setup(apiDocs, {
     swaggerOptions: {
       requestInterceptor: function (request) {
-        request.headers.Origin = `http://localhost:3333`;
+        request.headers.Origin = `${process.env.HOST}:${process.env.PORT}`;
         return request;
       },
-      url: `http://localhost:3333/docs/api-doc`,
+      url: `${process.env.HOST}:${process.env.PORT}/api/docs`,
     },
   })
 );
