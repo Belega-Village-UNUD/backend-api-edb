d563f2767c5af9dc27f4fb936adbc29a894cf9db
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-27 10:12:19 +0800

    EDP-77 Product Type models and migrations

---
+++ src/models/producttype.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class ProductType extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.Product, {
+        through: "ProductType",
+        as: "products",
+        foreignKey: "type_id",
+      });
+    }
+  }
+  ProductType.init(
+    {
+      name: DataTypes.STRING,
+      material: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "ProductType",
+    }
+  );
+  return ProductType;
+};
