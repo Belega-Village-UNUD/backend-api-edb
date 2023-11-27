6570c6f80582aff4f9958a441210ddcb6efbcdd9
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-27 13:33:56 +0800

    EDP-78 transaction models and migrations

---
+++ src/models/transaction.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasOne(models.DetailTransaction, {
+        foreignKey: "transaction_id",
+        as: "detail_transaction",
+      });
+      this.hasOne(models.Cart, {
+        foreignKey: "cart_id",
+        as: "cart",
+      });
+      this.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "user",
+      });
+      this.hasOne(models.Review, {
+        foreignKey: "transaction_id",
+        as: "review",
+      });
+    }
+  }
+  Transaction.init(
+    {
+      user_id: DataTypes.STRING,
+      cart_id: DataTypes.STRING,
+      total_amount: DataTypes.DECIMAL,
+      status: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Transaction",
+    }
+  );
+  return Transaction;
+};
