14bae0c48b2d4d496fad8b8cbd55be93542751e8
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-27 10:12:05 +0800

    EDP-77 store Models and migrations

---
+++ src/migrations/20231127014602-create-store.js
@@ -0,0 +1,57 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Store", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avatar_link: {
+        type: Sequelize.STRING,
+      },
+      image_link: {
+        type: Sequelize.STRING,
+      },
+      ktp_link: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      unverified_reason: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Store");
+  },
+};
