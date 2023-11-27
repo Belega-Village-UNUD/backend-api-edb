5b41a6d82de982fdc5e13cd054f31d66931d7501
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-27 10:33:28 +0800

    EDP-77 Cart models and migrations

---
+++ src/models/cart.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Product, {
+        foreignKey: "product_id",
+        as: "products",
+      });
+      this.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "users",
+      });
+    }
+  }
+  Cart.init(
+    {
+      user_id: DataTypes.STRING,
+      product_id: DataTypes.STRING,
+      qty: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Cart",
+    }
+  );
+  return Cart;
+};
