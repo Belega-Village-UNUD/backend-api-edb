cbc1e8f1058632b81a6539d2b3309c83e0f70473
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-11-28 19:44:25 +0800

    EDP-77 remove auto-increment serial

--- src/migrations/20231127043840-create-cancelled-transaction.js
+++ src/migrations/20231127043840-create-cancelled-transaction.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable("CancelledTransactions", {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.STRING,
       },
