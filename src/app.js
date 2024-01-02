e32a5cdb0cb56fcce2bd1fee5674f08d4ef79654
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-02 17:44:02 +0800

    EDP-103 initialize sentry

--- src/app.js
+++ src/app.js
@@ -13,6 +13,17 @@ const { response } = require("./utils/response.utils");
 const TIMEOUT = 5000;
 const app = express();
 
+Sentry.init({
+  dsn: process.env.DSN_SENTRY,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
