c44d26ad4614974310f81ec4098f069da2d96c17
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-03 16:36:36 +0800

    EDP-67 change to staging deployment

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
+
 git fetch;
-git pull origin EDP-67-sast-on-container;
-docker pull ghcr.io/belega-village-unud/backend-api-edb:edp-67-sast-on-container;
+git pull origin staging;
+docker pull ghcr.io/belega-village-unud/backend-api-edb:staging;
 make up;
 docker image prune;
 docker ps;
-
