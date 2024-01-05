d4c5ba5315581e98d497123f7f14d44b79328ea7
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-05 17:48:07 +0800

    EDP-67 merge command

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd backend-api-edb;
 git fetch;
 git pull origin staging;
 docker pull ghcr.io/belega-village-unud/backend-api-edb:staging;
 make up dns;
 docker image prune;
 docker ps;
+
