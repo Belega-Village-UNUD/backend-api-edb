60dc39f6a7bcb26608d68a9c2005159c90f4b8f9
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-05 16:41:42 +0800

    EDP-106 refactor: update path for upload avatar and add deletion path

--- src/controllers/profile/index.js
+++ src/controllers/profile/index.js
@@ -1,11 +1,13 @@
 const whoami = require("./whoami.controller");
 const updateUser = require("./update.controller");
 const deleteUser = require("./delete.controller");
-const upAvatar = require("./avatar.controller");
+const upAvatar = require("./avatar/upload.controller");
+const deleteAvatar = require("./avatar/delete.controller");
 
 module.exports = {
   whoami,
   updateUser,
   deleteUser,
   upAvatar,
+  deleteAvatar,
 };
