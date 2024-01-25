94220593e2756a44c6332f4d36d7f9b49927588b
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-25 15:08:08 +0800

    TODO test role seller

--- src/routes/product/type/type.route.js
+++ src/routes/product/type/type.route.js
@@ -1,36 +1,36 @@
 const router = require("express").Router();
 const controllers = require("../../../controllers/product");
 const middleware = require("../../../middlewares");
-const { MODULE } = require("../../../utils/enum.utils");
+const { MODULE, ROLE } = require("../../../utils/enum.utils");
 
 router.get(
   "/all",
   middleware.restrict,
-  middleware.rbac(MODULE.PRODUCT, true, true),
+  middleware.rbac(MODULE.PRODUCT, ROLE.SELLER, true, true),
   controllers.productType.getAllType
 );
 router.get(
   "/:id",
   middleware.restrict,
-  middleware.rbac(MODULE.PRODUCT, true, true),
+  middleware.rbac(MODULE.PRODUCT, ROLE.SELLER, true, true),
   controllers.productType.getOneType
 );
 router.post(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.PRODUCT, true, true),
+  middleware.rbac(MODULE.PRODUCT, ROLE.SELLER, true, true),
   controllers.productType.createType
 );
 router.put(
   "/:id",
   middleware.restrict,
-  middleware.rbac(MODULE.PRODUCT, true, true),
+  middleware.rbac(MODULE.PRODUCT, ROLE.SELLER, true, true),
   controllers.productType.updateType
 );
 router.delete(
   "/:id",
   middleware.restrict,
-  middleware.rbac(MODULE.PRODUCT, true, true),
+  middleware.rbac(MODULE.PRODUCT, ROLE.SELLER, true, true),
   controllers.productType.deleteType
 );
 
