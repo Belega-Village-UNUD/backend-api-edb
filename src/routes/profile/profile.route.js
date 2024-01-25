16a6e82319ee559cb49fc6b3065387868526e4d8
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-25 15:08:20 +0800

    TODO test role user

--- src/routes/profile/profile.route.js
+++ src/routes/profile/profile.route.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 const controller = require("../../controllers/profile");
 const middleware = require("../../middlewares");
-const { MODULE } = require("../../utils/enum.utils");
+const { MODULE, ROLE } = require("../../utils/enum.utils");
 const { image, document } = require("../../configs/multer.config");
 
 router.get(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.LANDING, true, false),
+  middleware.rbac(MODULE.LANDING, ROLE.USER, true, false),
   controller.whoami
 );
 router.put(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.LANDING, true, false),
+  middleware.rbac(MODULE.LANDING, ROLE.USER, true, false),
   controller.updateUser
 );
 router.post(
--- src/routes/shop/cart.route.js
+++ src/routes/shop/cart.route.js
@@ -1,33 +1,33 @@
 const router = require("express").Router();
 const controller = require("../../controllers/shop");
 const middleware = require("../../middlewares");
-const { MODULE } = require("../../utils/enum.utils");
+const { MODULE, ROLE } = require("../../utils/enum.utils");
 
 router.post(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.SHOP, true, true),
+  middleware.rbac(MODULE.SHOP, ROLE.USER, true, true),
   controller.addItem
 );
 
 router.get(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.SHOP, true, true),
+  middleware.rbac(MODULE.SHOP, ROLE.USER, true, true),
   controller.getItems
 );
 
 router.delete(
   "/",
   middleware.restrict,
-  middleware.rbac(MODULE.SHOP, true, true),
+  middleware.rbac(MODULE.SHOP, ROLE.USER, true, true),
   controller.removeItem
 );
 
 router.post(
   "/checkout",
   middleware.restrict,
-  middleware.rbac(MODULE.SHOP, true, true),
+  middleware.rbac(MODULE.SHOP, ROLE.USER, true, true),
   controller.checkoutItem
 );
 
