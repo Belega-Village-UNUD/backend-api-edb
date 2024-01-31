c6729e3ac2342abf5f5702e5b3f405cd7f736f61
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-31 16:58:43 +0000

    EDP-93 using docker stack for deploying instance

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git fetch;
-git pull origin staging;
-docker pull ghcr.io/belega-village-unud/backend-api-edb:staging;
-make up dns;
-docker image prune;
-docker ps;
+export $(cat .env) > /dev/null 2>&1; 
+docker stack deploy -c docker/service/docker-compose.yml ${1:-STACK_NAME}
+docker stack deploy -c docker/ssl/docker-compose.yml ${1:-STACK_NAME}
