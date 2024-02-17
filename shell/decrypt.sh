d214586511aaa5dd2d8f81c171220629fdff0900
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-02-17 22:29:04 +0800

    EDP-93 update decrypt shell

--- shell/decrypt.sh
+++ shell/decrypt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if ! command -v senv &> /dev/null
+if ! npx senv --version &> /dev/null
 then
-    echo "senv could not be found, please install senv https://github.com/jaydenwindle/sen://github.com/jaydenwindle/senv"
-    exit
-else 
-    senv decrypt .env.dev.enc -o .env.dev
+    echo "senv could not be found. Please install senv using npm or yarn."
+    echo "You can find more information at https://github.com/jaydenwindle/senv"
+    exit  1
+else  
+    if npx senv decrypt .env.dev.enc -o .env.dev
+    then
+        echo "Decryption successful. The decrypted file is .env.dev."
+    else
+        echo "Decryption failed. Please check the input file and ensure senv is working correctly."
+        exit  1
+    fi
 fi
-
