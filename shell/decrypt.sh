192534f7eeba728398fd0f1f63780c0380632ea1
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-02-17 22:34:07 +0800

    EDP-93 update to check senv path instead of running command if the process is slow

--- shell/decrypt.sh
+++ shell/decrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if ! npx senv --version &> /dev/null
+set -e 
+
+senv_path="./node_modules/.bin/senv"
+
+if [ -f "$senv_path" ] && [ -x "$senv_path" ]
 then
-    echo "senv could not be found. Please install senv using npm or yarn."
-    echo "You can find more information at https://github.com/jaydenwindle/senv"
-    exit  1
-else  
     if npx senv decrypt .env.dev.enc -o .env.dev
     then
         echo "Decryption successful. The decrypted file is .env.dev."
@@ -13,4 +13,8 @@ else
         echo "Decryption failed. Please check the input file and ensure senv is working correctly."
         exit  1
     fi
+else
+    echo "senv could not be found locally. Please install senv using npm or yarn."
+    echo "You can find more information at https://github.com/jaydenwindle/senv"
+    exit  1
 fi
