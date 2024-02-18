32b746045d38884b354cfc6c10f4ec17aa3f3801
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-02-18 23:23:14 +0800

    EDP-93 use docker-swarm-stack.yml file

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -8,16 +8,16 @@ fi
 
 export $(grep -v '^#' .env.dev | xargs)
 
-cp docker/service/docker-compose.yml docker/service/temp-docker-compose.yml
+cp docker/service/docker-swarm-stack.yml docker/service/temp-docker-swarm-stack.yml
 
 while IFS= read -r line; do
     var_name=$(echo "$line" | cut -d'=' -f1)
     var_value=$(printenv "$var_name")
     escaped_var_value="${var_value//\//\\/}"
-    sed -i "s/\${${var_name}}/${escaped_var_value}/g" docker/service/temp-docker-compose.yml
+    sed -i "s/\${${var_name}}/${escaped_var_value}/g" docker/service/temp-docker-swarm-stack.yml
 done < .env.dev
 
-sudo docker stack deploy -c docker/service/temp-docker-compose.yml backend_belega
+sudo docker stack deploy -c docker/service/temp-docker-swarm-stack.yml backend_belega
 
 # remove all the environment variables from the systems
 while IFS= read -r line; do
@@ -30,5 +30,5 @@ while IFS= read -r line; do
     fi
 done < .env.dev
 
-rm docker/service/temp-docker-compose.yml .env.dev
+rm docker/service/temp-docker-swarm-stack.yml .env.dev
 
