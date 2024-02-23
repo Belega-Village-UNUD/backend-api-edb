fbc4bd30732932b2e244927754c731205f49d6ac
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-02-23 19:28:01 +0800

    EDP-93 update nginx configuration

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -10,6 +10,7 @@ export $(grep -v '^#' .env.dev | xargs)
 
 cp docker/service/docker-swarm-stack.yml docker/service/temp-docker-swarm-stack.yml;
 cp docker/ssl/docker-compose.yml docker/ssl/temp-docker-compose.yml;
+cp nginx/conf.d/nginx.conf nginx/conf.d/temp-nginx.conf;
 
 while IFS= read -r line; do
     var_name=$(echo "$line" | cut -d'=' -f1)
@@ -23,6 +24,13 @@ while IFS= read -r line; do
     var_value=$(printenv "$var_name")
     escaped_var_value="${var_value//\//\\/}"
     sed -i "s/\${${var_name}}/${escaped_var_value}/g" docker/ssl/temp-docker-compose.yml
+done < .env.dev
+
+while IFS= read -r line; do
+    var_name=$(echo "$line" | cut -d'=' -f1)
+    var_value=$(printenv "$var_name")
+    escaped_var_value="${var_value//\//\\/}"
+    sed -i "s/\${${var_name}}/${escaped_var_value}/g" nginx/conf.d/nginx.conf
 done < .env.dev
 
 sudo docker stack deploy -c docker/service/temp-docker-swarm-stack.yml backend_belega
