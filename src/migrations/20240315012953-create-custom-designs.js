2f153a002627f065535d42c5a3d20b72ca92669f
justJooo <abel.jollando073@student.unud.ac.id>
2024-03-15 10:12:14 +0800

    EDP-100 add migration for custom design

---
+++ src/migrations/20240315012953-create-custom-designs.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("CustomDesigns", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      product_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image_link: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      height: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      width: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      budget: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("CustomDesigns");
+  },
+};
