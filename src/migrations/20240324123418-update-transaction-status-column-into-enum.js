b012988fd2c59bf69ec427c8d6911f04fcfd34a6
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-03-25 15:51:24 +0800

    EDP-79 add enum type

--- src/migrations/20240324123418-update-transaction-status-column-into-enum.js
+++ src/migrations/20240324123418-update-transaction-status-column-into-enum.js
@@ -9,6 +9,22 @@ module.exports = {
       allowNull: false,
       defaultValue: 0,
     });
+
+    await queryInterface.sequelize.query(
+      `DROP TYPE IF EXISTS "enum_Transactions_status";`
+    );
+    await queryInterface.sequelize.query(`
+      CREATE TYPE "enum_Transactions_status" AS ENUM('PENDING', 'PAYABLE', 'SUCCESS', 'CANCEL');
+    `);
+
+    await queryInterface.sequelize.query(`
+    await queryInterface.sequelize.query(`
+    await queryInterface.sequelize.query(`
+      DROP TYPE "enum_Transactions_status";
+    `);
+
     await queryInterface.removeColumn("Carts", "unit_price", {
       type: DataTypes.DECIMAL,
     });
+  },
+};
