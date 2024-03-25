bb5562c5ea980560b2349727ae1a1315ee439173
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-03-25 15:52:04 +0800

    EDP-79 alter column transactions from boolean to enum

--- src/migrations/20240324123418-update-transaction-status-column-into-enum.js
+++ src/migrations/20240324123418-update-transaction-status-column-into-enum.js
@@ -13,12 +13,42 @@ module.exports = {
     await queryInterface.sequelize.query(
       `DROP TYPE IF EXISTS "enum_Transactions_status";`
     );
+
+    await queryInterface.changeColumn("Transactions", "status", {
+      type: DataTypes.STRING,
+      allowNull: false,
+    });
+
     await queryInterface.sequelize.query(`
       CREATE TYPE "enum_Transactions_status" AS ENUM('PENDING', 'PAYABLE', 'SUCCESS', 'CANCEL');
     `);
 
     await queryInterface.sequelize.query(`
+      ALTER TABLE "Transactions"
+      ALTER COLUMN "status" TYPE "enum_Transactions_status"
+      USING (
+        CASE
+          WHEN "status" = 'true' THEN 'SUCCESS'::text::"enum_Transactions_status"
+          WHEN "status" = 'false' THEN 'PENDING'::text::"enum_Transactions_status"
+          ELSE 'CANCEL'::text::"enum_Transactions_status"
+        END
+      );
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.query(`
+      ALTER TABLE "Transactions"
+      ALTER COLUMN "status" TYPE BOOLEAN
+      USING (
+        CASE
+          WHEN "status" = 'SUCCESS' THEN true
+          WHEN "status" = 'PENDING' THEN false
+          ELSE false
+        END
+      );
+    `);
+
     await queryInterface.sequelize.query(`
       DROP TYPE "enum_Transactions_status";
     `);
