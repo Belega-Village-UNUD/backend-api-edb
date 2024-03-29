dabcf28bc403a13e99ab7054cbd893f4f542f68a
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-03-30 02:07:05 +0800

    EDP-159 update data model to using the product information and store informatino

--- src/controllers/shop/get-item.controller.js
+++ src/controllers/shop/get-item.controller.js
@@ -19,7 +19,37 @@ const getItems = async (req, res) => {
       ],
     });
 
-    return response(res, 200, true, "Cart items fetched", items);
+    const cartItems = await Promise.all(
+      items.map(async (item) => {
+        const { id, quantity, product_id } = item;
+        const product = await Product.findOne({
+          where: { id: product_id },
+          attributes: [
+            "id",
+            "image_product",
+            "name_product",
+            "price",
+            "stock",
+            "desc_product",
+            "type_id",
+          ],
+          include: [
+            {
+              model: Store,
+              as: "store",
+              attributes: ["id", "name", "phone", "address", "description"],
+            },
+          ],
+        });
+        return {
+          id,
+          quantity,
+          product,
+        };
+      })
+    );
+
+    return response(res, 200, true, "Cart items fetched", cartItems);
   } catch (error) {
     return response(res, error.status || 500, false, error.message, null);
   }
