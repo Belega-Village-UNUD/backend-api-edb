a0625c9355750d2980d70ae3356ff55ebfc04ccb
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-03-30 02:04:29 +0800

    EDP-159 use store id on defining the cart data based on users id

--- src/controllers/product/create.controller.js
+++ src/controllers/product/create.controller.js
@@ -26,11 +26,21 @@ const createProduct = async (req, res) => {
       return response(res, 404, false, "User not found", null);
     }
 
+    const store = await Store.findOne({ where: { user_id: user.id } });
+    if (!store) {
+      return response(
+        res,
+        404,
+        false,
+        "Store not found, please register as a seller first!",
+        null
+      );
+    }
     const { name_product, productTypeId, description, price, stock } = req.body;
     const product = await Product.create({
       id: nanoid(10),
       user_id: user.id,
-      store_id: store_id,
+      store_id: store.id,
       type_id: productTypeId,
       name_product: name_product,
       desc_product: description,
