92df184461904b9e4c3c471661500aa36a3b601a
justJooo <abel.jollando073@student.unud.ac.id>
2024-04-22 19:52:11 +0800

    EDP-171 fixing get item functionality

--- src/controllers/shop/get-item.controller.js
+++ src/controllers/shop/get-item.controller.js
@@ -1,5 +1,6 @@
-const { Cart, User, Product, Store } = require("../../models");
+const { Cart, User, Product, Store, Transaction } = require("../../models");
 const { response } = require("../../utils/response.utils");
+const Sequelize = require("sequelize");
 
 const getItems = async (req, res) => {
   try {
@@ -29,9 +30,37 @@ const getItems = async (req, res) => {
           ],
         },
       ],
+      raw: true,
     });
 
-    return response(res, 200, true, "Cart items fetched", cartItems);
+    const cartIds = cartItems;
+    console.log("🚀 ~ getItems ~ cartIds:", typeof cartIds);
+    // convert Object into array
+    //const cartIds = Object.keys(cartItems).map((key) => cartItems[key].id);
+    let arrayOfCartIds;
+    cartIds.forEach((element) => {
+      console.log("🚀 ~ cartIds.forEach ~ element:", element);
+      arrayOfCartIds = [...arrayOfCartIds, element.id];
+    });
+    console.log("🚀 ~ cartIds.forEach ~ arrayOfCartIds:", arrayOfCartIds);
+
+    const transactions = await Transaction.findAll({
+      where: { cart_id: { [Sequelize.Op.in]: arrayOfCartIds } },
+      attributes: ["id", "status"],
+    });
+
+    if (transactions.some((transaction) => transaction.status !== "PAID")) {
+      return response(res, 200, true, "Cart items fetched", cartItems);
+    }
+
+    //return response(res, 200, true, "Cart items fetched", cartItems);
+    return response(
+      res,
+      200,
+      true,
+      "You have no cart at the moment, please continue shopping",
+      null
+    );
   } catch (error) {
     return response(res, error.status || 500, false, error.message, null);
   }
