9721f622ce1e281cb7a315fcf49f95ade60a8c6d
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-04-23 15:21:46 +0800

    EDP-166 fix: disable deleting Cart

--- src/controllers/shop/checkout-item.controller.js
+++ src/controllers/shop/checkout-item.controller.js
@@ -2,83 +2,6 @@ const { nanoid } = require("nanoid");
 const { Cart, User, Product, Transaction } = require("../../models");
 const { response } = require("../../utils/response.utils");
 
-// const checkoutItem = async (req, res) => {
-//   try {
-//     const { id } = req.user;
-//     const user = await User.findOne({
-//       where: { id },
-//       attributes: { exclude: ["password"] },
-//     });
-
-//     const items = req.body;
-
-//     let totalAmount = 0;
-//     const cartIds = [];
-
-//     for (const item of items) {
-//       const { product_id, qty } = item;
-
-//       const product = await Product.findOne({ where: { id: product_id } });
-//       if (!product) {
-//         return response(res, 404, false, "Product not found", null);
-//       }
-
-//       if (qty > product.stock) {
-//         return response(
-//           res,
-//           400,
-//           false,
-//           `Insufficient stock for ${product.name_product}`,
-//           null
-//         );
-//       }
-
-//       if (qty < 1) {
-//         return response(
-//           res,
-//           400,
-//           false,
-//           `Minimum quantity for ${product.name_product} is 1`,
-//           null
-//         );
-//       }
-
-//       product.stock -= qty;
-//       await product.save();
-
-//       const cart = await Cart.findOne({
-//         where: { user_id: user.id, product_id },
-//       });
-//       if (!cart) {
-//         return response(res, 404, false, "Cart not found", null);
-//       }
-
-//       cartIds.push(cart.id);
-//       totalAmount += product.price * qty;
-//       if (cart.qty > qty) {
-//         cart.qty -= qty;
-//         await cart.save();
-//       } else {
-//         await Cart.destroy({ where: { user_id: user.id, product_id } });
-//       }
-//     }
-
-//     const transaction = await Transaction.create({
-//       id: nanoid(10),
-//       user_id: user.id,
-//       cart_id: cartIds,
-//       total_amount: totalAmount,
-//       status: "PENDING",
-//     });
-
-//     return response(res, 200, true, "Checkout successful", {
-//       totalAmount,
-//       transaction,
-//     });
-//   } catch (error) {
-//     return response(res, error.status || 500, false, error.message, null);
-//   }
-// };
 const checkoutItem = async (req, res) => {
   try {
     const { id } = req.user;
@@ -87,11 +10,11 @@ const checkoutItem = async (req, res) => {
       attributes: { exclude: ["password"] },
     });
 
-    const items = req.body;
+    const item = req.body;
 
     const transactions = [];
 
-    for (const item of items) {
+    //for (const item of items) {
       const { product_id, qty } = item;
 
       const product = await Product.findOne({ where: { id: product_id } });
@@ -133,9 +56,10 @@ const checkoutItem = async (req, res) => {
       if (cart.qty > qty) {
         cart.qty -= qty;
         await cart.save();
-      } else {
-        await Cart.destroy({ where: { user_id: user.id, product_id } });
       }
+      //else {
+      //  //await Cart.destroy({ where: { user_id: user.id, product_id } });
+      //}
 
       const transaction = await Transaction.create({
         id: nanoid(10),
@@ -149,10 +73,11 @@ const checkoutItem = async (req, res) => {
         totalAmount,
         transaction,
       });
-    }
+    //}
 
     return response(res, 200, true, "Checkout successful", transactions);
   } catch (error) {
+    console.log("🚀 ~ checkoutItem ~ error:", error);
     return response(res, error.status || 500, false, error.message, null);
   }
 };
