f4f0ff7b9f2f1062dfbfffc4470d5542d63cc2de
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-05-05 22:38:21 +0800

    EDP-133 controllers for `Buyer` transaction history
    TODO:
    
    - Create function for update status `PAID` so `Buyer` and `Seller` can get `DetailTransaction` data

--- src/controllers/transaction/index.js
+++ src/controllers/transaction/index.js
@@ -2,10 +2,14 @@ const getTokenMidtrans = require("./midtrans.controller");
 const confirmOrder = require("./confirmation/confirm.controller");
 const declineOrder = require("./confirmation/decline.controller");
 const getAllTransactions = require("./confirmation/get-all.controller");
+const getAllBuyerTransactionHistory = require("./buyer/get-all.controller");
+const getOneBuyerTransactionHistory = require("./buyer/get-one.controller");
 
 module.exports = {
   getTokenMidtrans,
   confirmOrder,
   declineOrder,
   getAllTransactions,
+  getAllBuyerTransactionHistory,
+  getOneBuyerTransactionHistory,
 };
