6cfe53c443239b5bd236b3d23a21fbf78d6a2523
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-05-05 22:32:25 +0800

    EDP-183 add buyer route for transactions
    - route for get all buyer transactions
    - route for get one buyer transaciions

--- src/routes/transaction/transaction.route.js
+++ src/routes/transaction/transaction.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("../../controllers/transaction");
 const middleware = require("../../middlewares");
+const buyer = require("./buyer/buyer.route");
 const { MODULE, ROLE } = require("../../utils/enum.utils");
 
 router.post("/", middleware.restrict, controller.getTokenMidtrans);
@@ -25,5 +26,7 @@ router.get(
   middleware.rbac(MODULE.TRANSACTION, ROLE.SELLER, true, true),
   controller.getAllTransactions
 );
+
+router.use("/buyer", buyer);
 
 module.exports = router;
