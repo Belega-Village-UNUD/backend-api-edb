b2d24112e2374daa3def6f4e329546b921c5d195
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-05-06 13:01:18 +0800

    EDP-186 finding `Transaction` modify ORM to find one and use params as identification

---
+++ src/controllers/transaction/buyer/pay.controller.js
@@ -0,0 +1,85 @@
+const Sequelize = require("sequelize");
+const { nanoid } = require("nanoid");
+const {
+  Transaction,
+  Cart,
+  Product,
+  User,
+  Store,
+  Profile,
+  DetailTransaction,
+} = require("../../../models");
+const { response } = require("../../../utils/response.utils");
+const sendEmail = require("./nodemailer.config");
+
+const payTransaction = async (req, res) => {
+  try {
+    const { id: user_id } = req.user;
+    const { id: transaction_id } = req.params;
+
+    const user = await User.findOne({ where: { id: user_id } });
+
+    let transaction = await Transaction.findOne({
+      attributes: ["id", "user_id", "status", "createdAt"],
+      include: [
+        {
+          model: Cart,
+          as: "cart",
+          attributes: ["id", "user_id", "product_id", "qty", "unit_price"],
+          include: [
+            {
+              model: User,
+              as: "user",
+              attributes: ["id", "email"],
+              include: [
+                {
+                  model: Profile,
+                  as: "userProfile",
+                  attributes: ["id", "name"],
+                },
+              ],
+            },
+            {
+              model: Product,
+              as: "product",
+              include: [
+                {
+                  model: Store,
+                  as: "store",
+                  attributes: ["id", "name"],
+                  include: [
+                    {
+                      model: User,
+                      as: "user",
+                      attributes: ["id", "email"],
+                      include: [
+                        {
+                          model: Profile,
+                          as: "userProfile",
+                          attributes: ["id", "name"],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      where: {
+        "$cart.user.id$": user.id,
+        id: transaction_id,
+      },
+    });
+
+    if (!transaction || transaction.length === 0) {
+      return response(
+        res,
+        200,
+        false,
+        "No transactions found for this user",
+        null
+      );
+    }
+
