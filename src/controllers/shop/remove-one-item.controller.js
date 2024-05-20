ef58d091c80e9632b75280fcf82811ce726c4e5f
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-05-20 23:55:18 +0800

    EDP-215 fix: use cart identifier

--- src/controllers/shop/remove-one-item.controller.js
+++ src/controllers/shop/remove-one-item.controller.js
@@ -1,10 +1,10 @@
-const { Cart, Product, User, Transaction } = require("../../models");
+const { Cart, Product, User, Store } = require("../../models");
 const { response } = require("../../utils/response.utils");
 
 const removeItem = async (req, res) => {
   try {
     const { id } = req.user;
-    const { product_id } = req.body;
+    const { cart_id } = req.body;
 
     const user = await User.findOne({
       where: { id },
@@ -15,21 +15,10 @@ const removeItem = async (req, res) => {
       return response(res, 404, false, "User not found", null);
     }
 
-    const transactions = await Transaction.findAll({
-      where: {
-        user_id: user.id,
-      },
-      attributes: ["id", "cart_id"],
-      raw: true,
-    });
-
     const cart = await Cart.findOne({
       where: {
-        id: {
-          [Op.notIn]: transactions.map((transaction) => transaction.cart_id),
-        },
+        id: cart_id,
         user_id: id,
-        product_id,
       },
       attributes: ["id", "qty"],
       include: [
@@ -66,14 +55,9 @@ const removeItem = async (req, res) => {
       return response(res, 404, false, "There's no product of this id", null);
     }
 
-    //if (cart.qty > qty) {
-    //  await cart.update({ qty: cart.qty - qty });
-    //  return response(res, 200, true, "Success reduce item", null);
-    //}
+    await Cart.destroy({ where: { id: cart.id } });
 
-    await Cart.delete({ where: { id: cart.id } });
-
-    return response(res, 200, true, "Your cart are removed", null);
+    return response(res, 200, true, "Successfully remove cart", null);
   } catch (error) {
     return response(res, error.status || 500, false, error.message, null);
   }
