f5e91f7e60dd3f4352fff3873be404bae7c6e785
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-05-26 15:03:32 +0800

    EDP-181 feat: use bash script to deploy

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+BRANCH=$1
+
 git fetch;
-git pull origin staging;
-docker pull ghcr.io/belega-village-unud/backend-api-edb:staging;
-make up dns;
-docker image prune;
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != $BRANCH ]; then
+    git checkout $BRANCH;
+fi
+
+git pull origin $BRANCH;
+docker image prune -f;
+make build-staging up;
+
+if [ $? -ne 0 ]; then
+    echo "Error in build $BRANCH and Deploy Backend Belega Service"
+fi
+
 docker ps;
