8e87ad718392548800e1869e7042e7572c7f8544
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-02 17:54:04 +0800

    EDP-225 feat: using weight and preorder value in creating product

--- src/controllers/product/seller/create.controller.js
+++ src/controllers/product/seller/create.controller.js
@@ -24,9 +24,25 @@ const createProduct = async (req, res) => {
         null
       );
     }
-    const { name_product, productTypeId, description, price, stock } = req.body;
+    const {
+      name_product,
+      productTypeId,
+      description,
+      price,
+      stock,
+      weight_gr,
+      is_preorder,
+    } = req.body;
 
-    if (!name_product || !productTypeId || !description || !price || !stock) {
+    if (
+      !name_product ||
+      !productTypeId ||
+      !description ||
+      !price ||
+      !stock ||
+      !weight_gr ||
+      is_preorder === null
+    ) {
       return response(res, 400, false, "Invalid input data", null);
     }
 
@@ -39,8 +55,10 @@ const createProduct = async (req, res) => {
       desc_product: description,
       price: price,
       stock: stock,
+      weight_gr: weight_gr,
+      is_preorder: is_preorder,
     });
-    return response(res, 200, true, `Product ${id} has been created`, product);
+    return response(res, 200, true, `Product has been created`, product);
   } catch (err) {
     return response(res, err.status || 500, false, err.message, null);
   }
