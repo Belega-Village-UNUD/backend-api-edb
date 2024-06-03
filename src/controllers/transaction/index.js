a253c3bfd40d9898b6b5222e6bb562762ce32508
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-03 18:34:49 +0800

    EDP-225 feat: add controller into index controller

--- src/controllers/transaction/index.js
+++ src/controllers/transaction/index.js
@@ -2,6 +2,7 @@ const getTokenMidtrans = require("./midtrans.controller");
 const confirmOrder = require("./confirmation/confirm.controller");
 const declineOrder = require("./confirmation/decline.controller");
 const getAllTransactions = require("./confirmation/get-all.controller");
+const getOneTransaction = require("./confirmation/get-one.controller");
 const getAllBuyerTransactionHistory = require("./buyer/get-all.controller");
 const getOneBuyerTransactionHistory = require("./buyer/get-one.controller");
 const payTransaction = require("./buyer/pay.controller");
@@ -12,6 +13,7 @@ module.exports = {
   confirmOrder,
   declineOrder,
   getAllTransactions,
+  getOneTransaction,
   getAllBuyerTransactionHistory,
   getOneBuyerTransactionHistory,
   payTransaction,
