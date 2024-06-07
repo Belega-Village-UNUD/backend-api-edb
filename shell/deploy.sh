7d1e86a7a595e2af0d25bd9857c93873b88476e9
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-07 21:53:25 +0800

    EDP-181 check branch

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -2,11 +2,13 @@
 
 BRANCH=$1
 
-git fetch;
+echo $BRANCH;
 
-if [ "$(git rev-parse --abbrev-ref HEAD)" != $BRANCH ]; then
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "$BRANCH" ]; then
     git checkout $BRANCH;
 fi
+
+git fetch;
 
 git pull origin $BRANCH;
 docker image prune -f;
@@ -15,5 +17,6 @@ make build-staging up;
 if [ $? -ne 0 ]; then
     echo "Error in build $BRANCH and Deploy Backend Belega Service"
 fi
+
 
 docker ps;
