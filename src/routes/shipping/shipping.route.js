34b16832ea71467aab6ad1091800cea868b33514
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-15 00:41:05 +0800

    EDP-228 add shipping route

---
+++ src/routes/shipping/shipping.route.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const controller = require("../../controllers");
+const middleware = require("../../middlewares");
+const { MODULE, ROLE } = require("../../utils/enum.utils");
+
+router.get(
+  "/city/:province_id",
+  middleware.restrict,
+  middleware.rbac(MODULE.MESSAGE, ROLE.BUYER, true, true),
+  middleware.buyerVerified,
+  controller.shipping.getCity
+);
+
+router.get(
+  "/province",
+  middleware.restrict,
+  middleware.rbac(MODULE.MESSAGE, ROLE.BUYER, true, true),
+  middleware.buyerVerified,
+  controller.shipping.getProvince
+);
+
+router.post(
+  "/costs/:transaction_id",
+  middleware.restrict,
+  middleware.rbac(MODULE.MESSAGE, ROLE.BUYER, true, true),
+  middleware.buyerVerified,
+  controller.shipping.costs
+);
+
+module.exports = router;
