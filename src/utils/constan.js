0ae0949bb3e868a0b117765c3989215a3c98ced7
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-15 00:38:59 +0800

    EDP-228 integration with Raja ongkir api

--- src/utils/constan.js
+++ src/utils/constan.js
@@ -1,3 +1,5 @@
 module.exports.MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
 module.exports.FE_URL = process.env.FE_URL;
 module.exports.MIDTRANS_APP_URL = process.env.MIDTRANS_APP_URL;
+module.exports.RAJAONGKIRAPI_URL = process.env.RAJAONGKIRAPI_URL;
+module.exports.RAJAONGKIRAPI_KEY = process.env.RAJAONGKIRAPI_KEY;
