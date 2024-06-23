2996ee8bfdf332d5bbfd915c2316677d993fb13d
dyudhani <ddickyumardhani@gmail.com>
2024-06-23 23:04:36 +0800

    EDP-257 update city and province

--- src/controllers/store/update-profile.controller.js
+++ src/controllers/store/update-profile.controller.js
@@ -1,36 +1,63 @@
 const { response } = require("../../utils/response.utils");
 const { Store, User } = require("../../models");
+const { readFileSyncJSON } = require("../../utils/file.utils");
 
 const updateProfileStore = async (req, res) => {
   try {
     const { id } = req.user;
-    const { name, address, description } = req.body;
+    const { name, address, description, city_id } = req.body;
 
-    const user = await User.findOne({ where: { id } });
+    if (!city_id) {
+      return response(res, 400, false, "Province and City is required", null);
+    }
+
+    let cityList = readFileSyncJSON("city.json");
+
+    const cityData = cityList.filter((item) => {
+      return item.city_id.toLowerCase() === city_id.toLowerCase();
+    });
+
+    const province = {
+      province_id: cityData[0].province_id,
+      province: cityData[0].province,
+    };
+    const city = cityData[0];
+
+    const data = {
+      name,
+      address,
+      description,
+      province,
+      city,
+    };
+
+    const user = await User.findOne({
+      where: { id },
+      attributes: { exclude: ["password"] },
+    });
     if (!user) {
       return response(res, 404, false, "User not found", null);
     }
+
     const store = await Store.findOne({ where: { user_id: user.id } });
     if (!store) {
       return response(res, 404, false, "Store not found", null);
     }
 
-    await Store.update(
-      { name, address, description },
-      { where: { user_id: user.id } }
-    );
+    await Store.update(data, { where: { user_id: user.id } });
+    const updatedStore = await Store.findOne({ where: { user_id: user.id } });
 
-    const updatedStore = await Store.findOne({
-      where: { user_id: user.id },
-      attributes: ["id", "name", "address", "description"],
-    });
+    const payload = {
+      user,
+      updatedStore,
+    };
 
     return response(
       res,
       200,
       true,
       `Successfully Update Store Profile ${store.name} `,
-      updatedStore
+      payload
     );
   } catch (err) {
     return response(res, err.status || 500, false, err.message, null);
