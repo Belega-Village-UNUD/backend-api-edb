2ef76fbb0c6ad1f3a818e16fb98aa3ac761bd9bd
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-06-23 18:05:35 +0800

    EDP-252: progress: seller transaction reports

--- src/utils/orm.utils.js
+++ src/utils/orm.utils.js
@@ -1,4 +1,5 @@
 const { Op } = require("sequelize");
+const sequelize = require("sequelize");
 const {
   Cart,
   User,
@@ -73,6 +74,68 @@ module.exports = {
 
     return carts;
   },
+  getCartsBasedOnStore: async (store_id) => {
+    const carts = await Cart.findAll({
+      attributes: ["id", "user_id", "product_id", "qty", "unit_price"],
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "email"],
+          include: [
+            {
+              model: Profile,
+              as: "userProfile",
+              attributes: ["id", "name", "city", "province"],
+            },
+          ],
+        },
+        {
+          model: Product,
+          as: "product",
+          include: [
+            {
+              model: ProductType,
+              as: "product_type",
+              attributes: ["name", "material"],
+            },
+            {
+              model: Store,
+              as: "store",
+              attributes: [
+                "id",
+                "name",
+                "phone",
+                "address",
+                "description",
+                "province",
+                "city",
+              ],
+              include: [
+                {
+                  model: User,
+                  as: "user",
+                  attributes: ["id", "email"],
+                  include: [
+                    {
+                      model: Profile,
+                      as: "userProfile",
+                      attributes: ["id", "name", "city", "province"],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      where: {
+        "$product.store.id$": store_id,
+      },
+    });
+
+    return carts;
+  },
   getDetailTransaction: async (transaction_id) => {
     const detail = await DetailTransaction.findOne({
       where: { transaction_id: transaction_id },
@@ -85,5 +148,51 @@ module.exports = {
     });
 
     return detail;
+  },
+  getStore: async (user_id) => {
+    const store = await Store.findOne({
+      where: { user_id: user_id },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "email"],
+          include: [
+            {
+              model: Profile,
+              as: "userProfile",
+              attributes: ["id", "name", "city", "province"],
+            },
+          ],
+        },
+      ],
+    });
+
+    return store;
+  },
+  getSalesReport: async (store_id) => {
+    const carts = await module.exports.getCartsBasedOnStore(store_id);
+    const cartIds = carts.map((cart) => cart.id);
+    console.log(cartIds.length);
+    const transactions = await Transaction.findAll({
+      where: {
+        status: "SUCCESS",
+        [Op.and]: sequelize.literal(
+          `cart_id && ARRAY[${cartIds
+            .map((id) => `'${id}'`)
+            .join(",")}]::varchar[]`
+        ),
+      },
+    });
+
+    const detailTransactions = await DetailTransaction.findAll({
+      where: {
+        transaction_id: {
+          [Op.in]: transactions.map((transaction) => transaction.id),
+        },
+      },
+    });
+
+    return transactions;
   },
 };
